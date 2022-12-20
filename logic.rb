1a0dcee48f8b5d17d522cc5539a679fc0c845f72
Vladislav <machnevvlad@gmail.com>
2022-12-20 19:13:31 +0400

    generation with multiple verbs

--- logic.rb
+++ logic.rb
@@ -1,5 +1,5 @@
 work_verb = {
-  "verb_root" => "работ", # бег, плав, куш, дум 
+  #"verb_root" => "работ", # бег, плав, куш, дум 
   "pretext_variants" => {
     "прошлое" => "",
     "настоящее" => "",
@@ -39,10 +39,11 @@ work_verb = {
     }
   }
 }
-
+ 
 actions = ["вопрос", "утверждение", "отрицание"]
 time_of_actions = ["будущее", "настоящее", "прошлое"]
 pronouns = ["я", "вы", "мы", "они", "он", "она"]
+array_verbs = ["работ", "бег", "плав", "куш", "дум"]
 #others = ["лето", "солнце", "кошек", "собак", "небо"]  // пока не применяется. добавлю в версии 2
 
 # метод для выбора случайного элемента из массива
@@ -50,34 +51,35 @@ def get_random_element(elements)
   elements.sample
 end
 
-def get_sentence(verb, actions, time_of_actions, pronouns)
+def get_sentence(verb, actions, time_of_actions, pronouns, array_verbs)
   action = get_random_element(actions)
   time_of_action = get_random_element(time_of_actions)
   pronoun = get_random_element(pronouns)
+  verb_root = get_random_element(array_verbs)
 
-  puts "time_of_action: #{time_of_action}" + " / " + "action: #{action}"
+  puts "time_of_action: #{time_of_action}" + " / " + "action: #{action}" + " / " + "verb: #{verb_root}"
 
   case action
   when "вопрос"
-    builded_verb = build_verb(verb, time_of_action, pronoun)
+    builded_verb = build_verb(verb, time_of_action, pronoun, verb_root)
     builded_pretext = build_pretext(verb, time_of_action, pronoun)
     sentence_constructor(action, pronoun, builded_verb, builded_pretext)
     
   when "утверждение"
-    builded_verb = build_verb(verb, time_of_action, pronoun)
+    builded_verb = build_verb(verb, time_of_action, pronoun, verb_root)
     builded_pretext = build_pretext(verb, time_of_action, pronoun)
     sentence_constructor(action, pronoun, builded_verb, builded_pretext)
     
   else
-    builded_verb = build_verb(verb, time_of_action, pronoun)
+    builded_verb = build_verb(verb, time_of_action, pronoun, verb_root)
     builded_pretext = build_pretext(verb, time_of_action, pronoun)
     sentence_constructor(action, pronoun, builded_verb, builded_pretext)
   end
 end
 
 # Метод собирает глагол с нужным окончанием
-def build_verb(verb, time_of_action, pronoun)
-  verb_root = verb["verb_root"]
+def build_verb(verb, time_of_action, pronoun, verb_root)
+  
   verb_ending = verb["pronoun_variants"][time_of_action][pronoun]
 
   verb_result = "#{verb_root}#{verb_ending}"
@@ -101,4 +103,4 @@ def sentence_constructor(action, pronoun, builded_verb, builded_pretext)
 end
 
 # puts "Генерация предложения на рус.языке. Версия 1:"
-puts get_sentence(work_verb, actions, time_of_actions, pronouns)
+puts get_sentence(work_verb, actions, time_of_actions, pronouns, array_verbs)
